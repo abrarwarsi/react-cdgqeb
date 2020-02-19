3d87ab193d767ea2323e63d05ff086ea6994fe41
abrarwarsi <abrarwarsi.13@gmail.com>
2020-02-19 19:17:05 +0530

    2 files modified

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './style.css';
 
 function App() {
   return (
     <div>
-      <h2>To-Do List</h2>
+      <h2 className="title">To-Do List</h2>
+      <input type="checkbox" />
+      <span>Some kind of task 1</span><br />
+      <input type="checkbox" />
+      <span>Some kind of task 2</span><br />
+      <input type="checkbox" />
+      <span>Some kind of task 3</span><br />
+      <input type="checkbox" />
+      <span>Some kind of task 4</span><br />
     </div>
   )
 }
