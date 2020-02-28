bf3ae19d8bc5c4f30f5c07a814e8f01a032faf45
abrarwarsi <abrarwarsi.13@gmail.com>
2020-02-28 12:11:20 +0530

    2 files modified, 1 added

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './style.css';
+import Joke from './Joke';
 
 function App() {
-const styles = {
-  color: "blue",
-  backgroundColor: "yellow"
-}
 
   return (
     <div>
-      <h2 style={styles}>To-Do List</h2>
-      <input type="checkbox" />
-      <span>Some kind of task 1</span><br />
-      <input type="checkbox" />
-      <span>Some kind of task 2</span><br />
-      <input type="checkbox" />
-      <span>Some kind of task 3</span><br />
-      <input type="checkbox" />
-      <span>Some kind of task 4</span><br />
+      <h2>Laugh if you find it funny!</h2>
+      <Joke 
+      question="What's the best thing about Switzerland?"
+      punchline="I don't know, but the flag is a big plus!"/>
+      <Joke
+      question="Did you hear about the mathematician who's afraid of negetive numbers?"
+      punchline="He'll stop at nothing to avoid them!"/>
+      <Joke
+      question="Hear about the new restaurant called Karma?"
+      punchline="There's no menu; you get what you deserve."/>
+      <Joke
+      question="Did you hear about the actor who fell through floorboards?"
+      punchline="He was just going through a stage."/>
+      <Joke
+      question="Did you hear about the claustrophobic astronaut?"
+      punchline="He just needed a little space."/>
     </div>
   )
 }
