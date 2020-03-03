45fbb3efc4e2ad2ef8f7a31abd49069a6fa9f99d
abrarwarsi <abrarwarsi.13@gmail.com>
2020-03-03 16:53:14 +0530

    1 file modified, 1 added

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class App extends Component() {
+  constructor () {
+    super();
+    this.state = {
+      name: "Yogesh",
+      age: 24
+    }
+  }
+  render () {
+    return (
+    <div>
+      <h1>{this.state.name}</h1>
+      <h3>{this.state.age} years old</h3>
+    </div>
+  )
+}
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,46 +1,7 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
-  render () {
-    return (
-    <div>
-      <Header />
-      <Greeting />
-    </div>
-  )
-}
-}
+import App from "./App";
 
-class Header extends Component {
-  render () {
-    return (
-    <header>
-      <p>Welcome, {this.props.username}!</p>
-    </header>
-  )
-  }
-}
-
-class Greeting extends Component {
-  render () {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
-
-  if (hours>=4 && hours<12) {
-    timeOfDay = "morning";
-  }
-  else if (hours>=12 && hours<16) {
-    timeOfDay = "afternoon";
-  }
-  else {
-    timeOfDay = "night"
-  }
-  return (
-    <h1>Good {timeOfDay} to you, sir or madam!</h1>
-  )
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
